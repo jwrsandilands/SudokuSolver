a5c78566d868801836b29596791c53901abe6c80
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2024-02-01 15:34:56 +0000

    Refactor application now prints grid of an actually solveable grid

--- src/main/java/com/example/sodukusolver/GridController.java
+++ src/main/java/com/example/sodukusolver/GridController.java
@@ -6,9 +6,13 @@ public class GridController {
     public void GenerateGrid(){
         grid = new int[9][9];
 
+        String numbers = "570010048081600075009700201094008102802106004060007890000073080308009000950840000";
+        int cellCounter = 0;
+
         for(int row = 0; row < grid.length; row++){
             for(int column = 0; column < grid.length; column++){
-                grid[row][column] = 1;
+                grid[row][column] = Character.getNumericValue(numbers.toCharArray()[cellCounter]);
+                cellCounter++;
             }
         }
     }
@@ -17,7 +21,12 @@ public class GridController {
         System.out.println("Your Generated Grid is:");
         for (int[] rows : grid) {
             for (int cell : rows) {
-                System.out.print(" " + rows[cell] + " ");
+                if(cell == 0){
+                    System.out.print(" " + "." + " ");
+                }
+                else{
+                    System.out.print(" " + cell + " ");
+                }
             }
             System.out.println();
         }
