5e31d13db3d88e8244afbc49331e1a2920abd27d
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2024-02-01 16:55:25 +0000

    Refactor Find Cell Sector; now finds number index of sector rather than coordinates

--- src/main/java/com/example/sodukusolver/GridController.java
+++ src/main/java/com/example/sodukusolver/GridController.java
@@ -33,13 +33,65 @@ public class GridController {
     }
 
     public void CalculateCellAnswers(int row, int column){
+        if(grid[column][row] != 0){
+            System.out.println("Your Cell Number is: " + grid[column][row]);
+        }
+
+        int sector = FindCellSector(row, column);
+
+        for(int number = 1; number < 9; number++){
+            CheckSectorForNumber(sector, number);
+        }
+    }
+
+    public int FindCellSector(int row, int column){
         int[] sectorCoords = new int[2];
         sectorCoords[0] = column/3;
         sectorCoords[1] = row/3;
 
-        System.out.println("Your Cell Number is:");
-        System.out.println(grid[column][row]);
-        System.out.println("Your Cell Sector is:");
-        System.out.println(sectorCoords[0] + " " + sectorCoords[1]);
+        int count = 1;
+        boolean countFound= false;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if((sectorCoords[0] == i) && (sectorCoords[1] == j)){
+                    countFound = true;
+                    break;
+                }
+                count++;
+            }
+            if(countFound){
+                break;
+            }
+        }
+
+        System.out.println("Your Cell Sector is: " + count);
+
+        return count;
+    }
+
+    public boolean CheckSectorForNumber(int sector, int number){
+        switch(sector){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 }
